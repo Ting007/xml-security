43f86896647c5d9a6ae134963aab818622ee2e2e
Ting007 <twa700@yahoo.com>
2017-08-18 15:56:25 -0400

    change the class path in build.sh file to jenkins environment

--- source/xml-security/build.sh
+++ source/xml-security/build.sh
@@ -2,9 +2,10 @@
 
 echo “reached build.sh”
 #CLASSPATH=/nfs/spectre/u5/aristot/subjects/lib:.
-CLASSPATH=/Users/ting/Doc/ComputerS/Test_objects/apache-xml-security/source:/Users/ting/Doc/ComputerS/Test_objects/apache-xml-security/source/xml-security:.
-CLASSPATH=$CLASSPATH:/Users/Shared/Jenkins/Home/workspace/test_xml_security/apache-xml-security/source/xml-security:/Users/Shared/Jenkins/Home/workspace/test_xml_security/source:.
-
+#CLASSPATH=/Users/ting/Doc/ComputerS/Test_objects/apache-xml-security/source:/Users/ting/Doc/ComputerS/Test_objects/apache-xml-security/source/xml-security:.
+#CLASSPATH=$CLASSPATH:/Users/Shared/Jenkins/Home/workspace/test_xml_security/apache-xml-security/source/xml-security:/Users/Shared/Jenkins/Home/workspace/test_xml_security/source:.
+CLASSPATH=$CLASSPATH:/Users/Shared/Jenkins/Home/workspace/apache-xml-security/source:/Users/Shared/Jenkins/Home/workspace/apache-xml-security/source/xml-security:.
+#export CLASSPATH
 export CLASSPATH
 
 ant ${1}
