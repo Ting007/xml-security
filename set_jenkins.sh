483a8ac04b96d59538fafebaec566a11c929a6ff
Ting007 <twa700@yahoo.com>
2017-08-23 14:30:43 -0400

    change the directory

--- set_jenkins.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-export JAVA_HOME=`/usr/libexec/java_home -v 1.6`
-export experiment_root=/Users/Shared/Jenkins/Home/workspace
-CLASSPATH=:/Users/Shared/Jenkins/Home/workspace/apache-xml-security/source:/Users/Shared/Jenkins/Home/workspace/apache-xml-security/source/xml-security.:$CLASSPATH
-export CLASSPATH
-
-cd scripts
-./install.sh orig 0
