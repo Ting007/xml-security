c93f960e217bede9a64b46f0e3eb7c18a57625b0
Ting007 <twa700@yahoo.com>
2017-08-28 16:10:30 -0400

    changed run_jenkins.sh file add java home setting

--- scripts/run_jenkins.sh
+++ scripts/run_jenkins.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-#export JAVA_HOME=`/usr/libexec/java_home -v 1.6`
+export JAVA_HOME=`/usr/libexec/java_home -v 1.6`
 export experiment_root=/Users/Shared/Jenkins/Home/workspace
 CLASSPATH=:/Users/Shared/Jenkins/Home/workspace/apache-xml-security/source:/Users/Shared/Jenkins/Home/workspace/apache-xml-security/source/xml-security.
 export CLASSPATH
-echo $JAVA_HOME
-echo $CLASSPATH
+echo "JAVA_HOME is " $JAVA_HOME
+echo "CLASSPATH is " $CLASSPATH
 ./install.sh orig 0
