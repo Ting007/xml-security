880553cf49c638e0d603b490a9a134d47d018478
Ting007 <twa700@yahoo.com>
2017-08-28 17:15:40 -0400

    change the chmod of runTestss.sh

--- source/xml-security/runTests.sh
+++ source/xml-security/runTests.sh
@@ -1,16 +1,10 @@
 #!/bin/sh
 CLASSPATH="$CLASSPATH":./build/classes:../../common/libs
-echo "CLASSPATH is " $CLASSPATH
-
 
 for i in ../../common/libs/*.jar
 do
     CLASSPATH=$CLASSPATH:$i
 done
 
-export CLASSPATH
+$JAVA_HOME/bin/java $* org.apache.xml.security.test.AllTests
 
-#$JAVA_HOME/bin/java $* org.apache.xml.security.test.AllTests
-echo "JAVA_HOME is " $JAVA_HOME
-
-$JAVA_HOME/bin/java -version
