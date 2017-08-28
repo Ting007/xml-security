8c99c270170513f4c1c2fcb158b08ee6f81601aa
Ting007 <twa700@yahoo.com>
2017-08-28 16:07:20 -0400

    changed run_jenkins.sh file

--- scripts/run_jenkins.sh
+++ scripts/run_jenkins.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
+#export JAVA_HOME=`/usr/libexec/java_home -v 1.6`
+export experiment_root=/Users/Shared/Jenkins/Home/workspace
+CLASSPATH=:/Users/Shared/Jenkins/Home/workspace/apache-xml-security/source:/Users/Shared/Jenkins/Home/workspace/apache-xml-security/source/xml-security.
+export CLASSPATH
+echo $JAVA_HOME
+echo $CLASSPATH
 ./install.sh orig 0
