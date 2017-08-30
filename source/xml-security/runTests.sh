cb56bd02096b1303d9cf43c4d1c4efa1a8ce31f2
Ting007 <twa700@yahoo.com>
2017-08-30 16:31:09 -0400

    add classpath to runtest.sh

--- source/xml-security/runTests.sh
+++ source/xml-security/runTests.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+export JAVA_HOME=`/usr/libexec/java_home -v 1.6`
+export experiment_root=/Users/Shared/Jenkins/Home/workspace
+CLASSPATH=:/Users/Shared/Jenkins/Home/workspace/apache-xml-security/source:/Users/Shared/Jenkins/Home/workspace/apache-xml-security/source/xml-security.
+export CLASSPATH
+
 CLASSPATH="$CLASSPATH":./build/classes:../../common/libs
 
 for i in ../../common/libs/*.jar
