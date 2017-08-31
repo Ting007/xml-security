def1615b2aca56371629882467bd449b24c52d40
Ting007 <twa700@yahoo.com>
2017-08-31 09:58:24 -0400

    put build and runTest in the same scripts

--- scripts/run_jenkins.sh
+++ scripts/run_jenkins.sh
@@ -3,6 +3,10 @@ export JAVA_HOME=`/usr/libexec/java_home -v 1.6`
 export experiment_root=/Users/Shared/Jenkins/Home/workspace
 CLASSPATH=:/Users/Shared/Jenkins/Home/workspace/apache-xml-security/source:/Users/Shared/Jenkins/Home/workspace/apache-xml-security/source/xml-security.
 export CLASSPATH
-echo "JAVA_HOME is " $JAVA_HOME
-echo "CLASSPATH is " $CLASSPATH
+
 ./install.sh orig 0
+
+#running the test suits 
+cd ../source/xml-security
+./runTests.sh
+./scriptR0.cls
