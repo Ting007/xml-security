a4d98e17bea393e8dbae4a8a6c684e4e0e17da5e
Ting007 <twa700@yahoo.com>
2017-09-06 16:49:13 -0400

    change the set_jenkins.sh

--- scripts/set_jenkins.sh
+++ scripts/set_jenkins.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 export JAVA_HOME=`/usr/libexec/java_home -v 1.6`
 export experiment_root=/Users/Shared/Jenkins/Home/workspace
-CLASSPATH=:/Users/Shared/Jenkins/Home/workspace/apache-xml-security/source:/Users/Shared/Jenkins/Home/workspace/apache-xml-security/source/xml-security.
+CLASSPATH=.:/Users/Shared/Jenkins/Home/workspace/apache-xml-security/source:/Users/Shared/Jenkins/Home/workspace/apache-xml-security/source/xml-security
 export CLASSPATH
