27a4976ec55d68d31970c7ece7d5553bcd8f60d9
Ting007 <twa700@yahoo.com>
2017-09-15 11:34:11 -0400

    with the jacoco excel file generated

--- source/xml-security/runTests.sh
+++ source/xml-security/runTests.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
-export JAVA_HOME=`/usr/libexec/java_home -v 1.6`
-export experiment_root=/Users/Shared/Jenkins/Home/workspace
-CLASSPATH=:/Users/Shared/Jenkins/Home/workspace/apache-xml-security/source:/Users/Shared/Jenkins/Home/workspace/apache-xml-security/source/xml-security.
-export CLASSPATH
 
-CLASSPATH="$CLASSPATH":./build/classes:../../common/libs
+export JAVA_HOME=`/usr/libexec/java_home -v 1.6`
+#export experiment_root=/Users/Shared/Jenkins/Home/workspace
+#CLASSPATH=:${experiment_root}/apache-xml-security/source:${experiment_root}/apache-xml-security/source/xml-security.
+
+CLASSPATH="$CLASSPATH":./build/classes/:../../common/libs
 
 for i in ../../common/libs/*.jar
 do
     CLASSPATH=$CLASSPATH:$i
 done
 
-$JAVA_HOME/bin/java $* org.apache.xml.security.test.AllTests
+export CLASSPATH
 
+$JAVA_HOME/bin/java $* org.apache.xml.security.test.AllTests
